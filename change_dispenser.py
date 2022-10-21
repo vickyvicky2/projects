0ae2f1732af519aa086925b3ce346dd505450211
Vicky Aguilar <80185615+vickyvicky2@users.noreply.github.com>
2022-10-21 17:11:27 -0400

    Update change_dispenser.py

--- change_dispenser.py
+++ change_dispenser.py
@@ -4,14 +4,7 @@
 #
 # DESCRIPTION
 #  This program prompts for a number from 0 to 99, and then outputs that number in quarters, dimes, nickles and pennies
-#  
-# DATE
-#  due October 14, 2022
-#
-# PROGRAMMER
-# Victoria Aguilar
-# aguil113@canton.edu
-#
+
 # prompts user to enter a number to be converted
 number = eval(input("Please enter a number between 0 to 99 to be converted into quarters, dimes and nickles: "))
 
